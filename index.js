9f8876fa7469483a92c5ed72e0d754d3944cd1eb
= <anagha.patil@educrack.com>
2022-04-05 19:35:46 +0530

    jhds

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
-// var AWS = require("aws-sdk");
+const express = require('express')
+const app = express()
+const port = 3000
+var AWS = require("aws-sdk");
+// Set the region 
+AWS.config.update({region: 'REGION'});
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+// Create EC2 service object
+var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+var params = {
+  DryRun: false
+};
+app.get('/', (req, res) => {
+  //res.send('Hello World!')
+  ec2.describeInstances(params, function(err, data) {
+    if (err) {
+      res.send("Error", err.stack);
+    } else {
+      res.send("Success", JSON.stringify(data));
+    }
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 // Load the AWS SDK for Node.js
 // var AWS = require('aws-sdk');
@@ -31,54 +46,3 @@
 //     console.log("Success", JSON.stringify(data));
 //   }
 // });
-
-
-
-// Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({region: 'us-east-1'});
-
-// Create EC2 service object
-var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
-
-var params = {
-  InstanceIds: ['i-0b36f38f5c4148eed'],
-  DryRun: true
-};
-
-if (process.argv[2].toUpperCase() === "ON") {
-  console.log(process.argv[2])
-  // Call EC2 to start monitoring the selected instances
-  ec2.monitorInstances(params, function(err, data) {
-    console.log("jj=",err)
-    if (err && err.code === 'DryRunOperation') {
-      params.DryRun = false;
-      ec2.monitorInstances(params, function(err, data) {
-          if (err) {
-            console.log("Error", err);
-          } else if (data) {
-            console.log("Success", data.InstanceMonitorings);
-          }
-      });
-    } else {
-      console.log("You don't have permission to change instance monitoring.");
-    }
-  });
-} else if (process.argv[2].toUpperCase() === "OFF") {
-  // Call EC2 to stop monitoring the selected instances
-  ec2.unmonitorInstances(params, function(err, data) {
-    if (err && err.code === 'DryRunOperation') {
-      params.DryRun = false;
-      ec2.unmonitorInstances(params, function(err, data) {
-          if (err) {
-            console.log("Error", err);
-          } else if (data) {
-            console.log("Success", data.InstanceMonitorings);
-          }
-      });
-    } else {
-      console.log("You don't have permission to change instance monitoring.");
-    }
-  });
-}
