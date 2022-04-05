1ea95c6e5c875c809c6a81607f2b714c6e10174a
= <anagha.patil@educrack.com>
2022-04-05 19:30:11 +0530

    jhds

--- index.js
+++ index.js
@@ -37,7 +37,7 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'REGION'});
+AWS.config.update({region: 'us-east-1'});
 
 // Create EC2 service object
 var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
