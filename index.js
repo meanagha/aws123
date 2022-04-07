7ad1d4f2f5df022bf5b5c177b16d044db89b41f1
= <anagha.patil@educrack.com>
2022-04-07 12:25:46 +0530

    kk

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000
 var AWS = require("aws-sdk");
 // Set the region 
 AWS.config.update({region: 'us-east-1'});
+var meta  = new AWS.MetadataService();
 
 // Create EC2 service object
 var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
@@ -12,17 +13,19 @@ var params = {
   DryRun: false
 };
 app.get('/', (req, res) => {
-  
+ 
   ec2.describeInstances(params, function(err, data) {
     if (err) {
       console.log("Error", err.stack);
     } else {
-      console.log("Success", JSON.stringify(data));
+      console.log("Success", JSON.stringify(data.InstanceId));
       res.send('Hello World!')
     }
   });
 })
-
+meta.request("/latest/meta-data/instance-id", function(err, data){
+  console.log(data);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
